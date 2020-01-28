a0531cd52284bf10638553ee822531cf62911172
RIDWAN RAJI <ror716@mail.usask.ca>
2020-01-27 19:02:19 -0600

    load random counsel is now working

--- src/App.js
+++ src/App.js
@@ -10,14 +10,55 @@ class App extends React.Component {
     this.state = {
       error: null,
       isLoaded: false,
-      counsels: []
+      counsels: [],
+      oneCounsel: {}
     };
   }
   componentDidMount() {
-    let word = "melody";
+    let word = "me";
+
+    // Load initial counsel from the API
+    this.loadRandomCounsel();
+
+    // Load random counsel from the API at specific interval
+    setInterval(async () => {
+      this.loadRandomCounsel();
+    }, 60000);
     this.searchCounsel(word);
   }
 
+  /**
+   * A function that Generate random counsel from API
+   */
+  async loadRandomCounsel() {
+    try {
+      fetch("https://api.adviceslip.com/advice")
+        .then(response2 => response2.json())
+        .then(
+          data2 => {
+            this.setState({
+              isLoaded: true,
+              oneCounsel: data2.slip
+            });
+          },
+          error => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  /**
+   * A function that search keyword from the API.
+   * If keyword is found, return an array object conataining 
+   * the corresponding keywords 
+   * @param {*} x Keyword to search from the API
+   */
   searchCounsel(x) {
     fetch(`https://api.adviceslip.com/advice/search/${x}`)
       .then(response => response.json())
@@ -35,22 +76,33 @@ class App extends React.Component {
           });
         }
       );
+    const counselList = this.state.counsels.map(item => (
+      <Counsel key={item.slip_id} counsel={item.advice} />
+    ));
+    return (
+      <div className="App">
+        <p> {counselList}</p>
+      </div>
+    );
   }
 
   render() {
-    console.log(this.state.counsels);
+    // console.log(this.state.counsels);
+    // console.log(this.state.oneCounsel)
 
     if (this.state.error) {
       return <div className="App">Error: {this.state.error.message}</div>;
     } else if (!this.state.isLoaded) {
       return <div className="App">Loading...</div>;
     } else {
-      const counselData = this.state.counsels.map(item => (
-        <Counsel key={item.slip_id} counsel={item.advice} />
-      ));
+      const singleCounsel = (
+        <div>
+          {this.state.oneCounsel.advice}
+        </div>
+      );
       return (
         <div className="App">
-          <header className="App-header">{counselData}</header>
+          <h3 className="App-header">{singleCounsel}</h3>
         </div>
       );
     }
