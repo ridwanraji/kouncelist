f929fe218d5608c3200d301931040748f76d01f5
RIDWAN RAJI <ror716@mail.usask.ca>
2020-01-29 13:02:47 -0600

    remove emoji component style

--- src/components/Emoji.js
+++ src/components/Emoji.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "../Styles/App.css";
+
 const Emoji = props => (
   <span
-    className="emoji"
     role="img"
     aria-label={props.label ? props.label : ""}
     aria-hidden={props.label ? "false" : "true"}
